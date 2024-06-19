9689edfb1ec53504f243de87048dc868b18cb482
stefanysouza9 <stefanymatosdesouza9@gmail.com>
2024-06-19 08:52:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.queryCommandValue(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1,"
+        alternativas: [
+            "Alternativa 1",
+        ]
+    }
+]
